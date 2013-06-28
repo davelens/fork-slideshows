201057fb4863906671bf9498c852d64ba132e8fe
davelens <github@davelens.be>
2013-06-28 23:21:57 +0200

    add a comment to the generated slideshows.php file

--- backend/modules/slideshows/engine/helper.php
+++ backend/modules/slideshows/engine/helper.php
@@ -246,6 +246,8 @@ class Frontend' . $camelcasedModule . 'SlideshowsModel
 	{
 		$db = FrontendModel::getContainer()->get(\'database\');
 
+		// This should work with an interface so people know what fields to add.
+		// For now, check slideshows/layout/templates/basic.tpl to mimick the array structure.
 		$records = array();
 
 		return $records;
